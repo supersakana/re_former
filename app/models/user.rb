d39210af81bf885a131d063fe56092b07c027e5f
supersakana <zacwilliamson@icloud.com>
2022-08-20 13:02:41 -0600

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+    validates :email, presence: true, uniqueness: true #, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { in: 6..25 }
 end
